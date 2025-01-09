2ada9eb312a746d570258c2bf1a84363dea51b98
su <tndyd0327@gmail.com>
2025-01-09 11:43:13 +0900

    repo 추가

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,17 +1,18 @@
 import { createContext, useState } from "react";
 
 const GithubContext = createContext();
-
-// 깃허브주소와 토큰 변수 지정
+// 깃허브주소와 토큰 변수지정
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-// 프로바이더가 전역으로 컨텍스트를 적용함
+//프로바이더가 전역으로 컨텍스트를 적용함
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
+  const [repos, setRepos] = useState([]);
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
-
+  //유저들 클리어
+  const clearUsers = () => setUsers([]);
   //키워드로 유저찾기
   const searchUsers = (text) => {
     setLoading(true);
@@ -24,14 +25,14 @@ export const GithubProvider = ({ children }) => {
       .then((res) => res.json())
       .then((data) => {
         setUsers(data.items);
-        setLoading(false); //로딩 완료!
+        setLoading(false); //로딩완료!
       })
       .catch((err) => console.log(err));
   };
-
-  const getUser = (loiong) => {
+  //깃허브아이디로 유저찾기
+  const getUser = (login) => {
     setLoading(true);
-    fetch(`${GITHUB_URL}/search/users?${loiong}`, {
+    fetch(`${GITHUB_URL}/users/${login}`, {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
       },
@@ -39,17 +40,40 @@ export const GithubProvider = ({ children }) => {
       .then((res) => res.json())
       .then((data) => {
         setUser(data);
-        setLoading(false); //로딩 완료!
+        setLoading(false); //로딩완료!
       })
-      .catch((err) => (window.location = "/notFound"));
+      .catch((err) => (window.location = "/notfound"));
+    getUserRepos(login);
+  };
+  //유저 공개리포 리스트
+  const getUserRepos = (login) => {
+    setLoading(true);
+    const pararms = new URLSearchParams({
+      sort: "created", //생성된 날짜 순으로 설정
+      per_page: 10,
+    });
+    fetch(`${GITHUB_URL}/users/${login}/repos?${pararms}`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setRepos(data);
+        setLoading(false); //로딩완료!
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <GithubContext.Provider
       value={{
         users,
+        repos,
+        user,
         loading,
         searchUsers,
         getUser,
+        clearUsers,
       }}
     >
       {children}
