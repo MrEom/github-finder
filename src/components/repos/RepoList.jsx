dc854e62b7643f7ff4831b63f02e316811eff965
su <tndyd0327@gmail.com>
2025-01-09 13:54:51 +0900

    최종

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -8,9 +8,8 @@ function RepoList({ repos }) {
         <h2 className="text-3xl my-4 font-bold card-title">
           최근 리포 10개 표시
         </h2>
-        {repos.map((repo) => (
-          <RepoItem key={repo.id} repo={repo} />
-        ))}
+        {repos.length > 0 &&
+          repos.map((repo) => <RepoItem key={repo.id} repo={repo} />)}
       </div>
     </div>
   );
